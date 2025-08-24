796824ed6c2a2efce70cc3299463b38356fb11a3
rajexploit404 <139728058+rajexploit404@users.noreply.github.com>
2025-08-24 14:02:02 +0700

    Update app.py

--- app.py
+++ app.py
@@ -185,4 +185,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__=="__main__":
-    app.run(debug=True, host="0.0.0.0", port=65512)
+    app.run(debug=True, host="0.0.0.0", port=5000)
